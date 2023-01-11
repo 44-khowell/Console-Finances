5708a3d35c0dd9882615a052a40b2755a5042e06
kh.saint44 <119610043+44-khowell@users.noreply.github.com>
2023-01-11 00:51:50 +0000

    removed script.js used for testing code and renamed script-org.js to script.js

--- script.js
+++ script.js
@@ -1,6 +1,183 @@
+var finances = [
+  ['Jan-2010', 867884],
+  ['Feb-2010', 984655],
+  ['Mar-2010', 322013],
+  ['Apr-2010', -69417],
+  ['May-2010', 310503],
+  ['Jun-2010', 522857],
+  ['Jul-2010', 1033096],
+  ['Aug-2010', 604885],
+  ['Sep-2010', -216386],
+  ['Oct-2010', 477532],
+  ['Nov-2010', 893810],
+  ['Dec-2010', -80353],
+  ['Jan-2011', 779806],
+  ['Feb-2011', -335203],
+  ['Mar-2011', 697845],
+  ['Apr-2011', 793163],
+  ['May-2011', 485070],
+  ['Jun-2011', 584122],
+  ['Jul-2011', 62729],
+  ['Aug-2011', 668179],
+  ['Sep-2011', 899906],
+  ['Oct-2011', 834719],
+  ['Nov-2011', 132003],
+  ['Dec-2011', 309978],
+  ['Jan-2012', -755566],
+  ['Feb-2012', 1170593],
+  ['Mar-2012', 252788],
+  ['Apr-2012', 1151518],
+  ['May-2012', 817256],
+  ['Jun-2012', 570757],
+  ['Jul-2012', 506702],
+  ['Aug-2012', -1022534],
+  ['Sep-2012', 475062],
+  ['Oct-2012', 779976],
+  ['Nov-2012', 144175],
+  ['Dec-2012', 542494],
+  ['Jan-2013', 359333],
+  ['Feb-2013', 321469],
+  ['Mar-2013', 67780],
+  ['Apr-2013', 471435],
+  ['May-2013', 565603],
+  ['Jun-2013', 872480],
+  ['Jul-2013', 789480],
+  ['Aug-2013', 999942],
+  ['Sep-2013', -1196225],
+  ['Oct-2013', 268997],
+  ['Nov-2013', -687986],
+  ['Dec-2013', 1150461],
+  ['Jan-2014', 682458],
+  ['Feb-2014', 617856],
+  ['Mar-2014', 824098],
+  ['Apr-2014', 581943],
+  ['May-2014', 132864],
+  ['Jun-2014', 448062],
+  ['Jul-2014', 689161],
+  ['Aug-2014', 800701],
+  ['Sep-2014', 1166643],
+  ['Oct-2014', 947333],
+  ['Nov-2014', 578668],
+  ['Dec-2014', 988505],
+  ['Jan-2015', 1139715],
+  ['Feb-2015', 1029471],
+  ['Mar-2015', 687533],
+  ['Apr-2015', -524626],
+  ['May-2015', 158620],
+  ['Jun-2015', 87795],
+  ['Jul-2015', 423389],
+  ['Aug-2015', 840723],
+  ['Sep-2015', 568529],
+  ['Oct-2015', 332067],
+  ['Nov-2015', 989499],
+  ['Dec-2015', 778237],
+  ['Jan-2016', 650000],
+  ['Feb-2016', -1100387],
+  ['Mar-2016', -174946],
+  ['Apr-2016', 757143],
+  ['May-2016', 445709],
+  ['Jun-2016', 712961],
+  ['Jul-2016', -1163797],
+  ['Aug-2016', 569899],
+  ['Sep-2016', 768450],
+  ['Oct-2016', 102685],
+  ['Nov-2016', 795914],
+  ['Dec-2016', 60988],
+  ['Jan-2017', 138230],
+  ['Feb-2017', 671099],
+];
+
+var financesUpdate = [
+  ['Jan-2010', 867884],
+  ['Feb-2010', 984655],
+  ['Mar-2010', 322013],
+  ['Apr-2010', -69417],
+  ['May-2010', 310503],
+  ['Jun-2010', 522857],
+  ['Jul-2010', 1033096],
+  ['Aug-2010', 604885],
+  ['Sep-2010', -216386],
+  ['Oct-2010', 477532],
+  ['Nov-2010', 893810],
+  ['Dec-2010', -80353],
+  ['Jan-2011', 779806],
+  ['Feb-2011', -335203],
+  ['Mar-2011', 697845],
+  ['Apr-2011', 793163],
+  ['May-2011', 485070],
+  ['Jun-2011', 584122],
+  ['Jul-2011', 62729],
+  ['Aug-2011', 668179],
+  ['Sep-2011', 899906],
+  ['Oct-2011', 834719],
+  ['Nov-2011', 132003],
+  ['Dec-2011', 309978],
+  ['Jan-2012', -755566],
+  ['Feb-2012', 1170593],
+  ['Mar-2012', 252788],
+  ['Apr-2012', 1151518],
+  ['May-2012', 817256],
+  ['Jun-2012', 570757],
+  ['Jul-2012', 506702],
+  ['Aug-2012', -1022534],
+  ['Sep-2012', 475062],
+  ['Oct-2012', 779976],
+  ['Nov-2012', 144175],
+  ['Dec-2012', 542494],
+  ['Jan-2013', 359333],
+  ['Feb-2013', 321469],
+  ['Mar-2013', 67780],
+  ['Apr-2013', 471435],
+  ['May-2013', 565603],
+  ['Jun-2013', 872480],
+  ['Jul-2013', 789480],
+  ['Aug-2013', 999942],
+  ['Sep-2013', -1196225],
+  ['Oct-2013', 268997],
+  ['Nov-2013', -687986],
+  ['Dec-2013', 1150461],
+  ['Jan-2014', 682458],
+  ['Feb-2014', 617856],
+  ['Mar-2014', 824098],
+  ['Apr-2014', 581943],
+  ['May-2014', 132864],
+  ['Jun-2014', 448062],
+  ['Jul-2014', 689161],
+  ['Aug-2014', 800701],
+  ['Sep-2014', 1166643],
+  ['Oct-2014', 947333],
+  ['Nov-2014', 578668],
+  ['Dec-2014', 988505],
+  ['Jan-2015', 1139715],
+  ['Feb-2015', 1029471],
+  ['Mar-2015', 687533],
+  ['Apr-2015', -524626],
+  ['May-2015', 158620],
+  ['Jun-2015', 87795],
+  ['Jul-2015', 423389],
+  ['Aug-2015', 840723],
+  ['Sep-2015', 568529],
+  ['Oct-2015', 332067],
+  ['Nov-2015', 989499],
+  ['Dec-2015', 778237],
+  ['Jan-2016', 650000],
+  ['Feb-2016', -1100387],
+  ['Mar-2016', -174946],
+  ['Apr-2016', 757143],
+  ['May-2016', 445709],
+  ['Jun-2016', 712961],
+  ['Jul-2016', -1163797],
+  ['Aug-2016', 569899],
+  ['Sep-2016', 768450],
+  ['Oct-2016', 102685],
+  ['Nov-2016', 795914],
+  ['Dec-2016', 60988],
+  ['Jan-2017', 138230],
+  ['Feb-2017', 671099],
+];
 
 
-// Start of JavaScript Code --- Test File
+// Start of JavaScript Code 
 
 
 // Create an individual array to hold the Months values
@@ -10,42 +187,10 @@
 // Using the months array, count the total number of months which is held within - thus display in console 
 
 // Using the Profits & Loss array, sum up the Total of Profits (+ve elements) over the period - thus display in console
-
+   
 // Using the Profits & Loss array, create a Profit_Loss_Change array
 
-
-
-
-
-// let array2D = [[1, 2, 3], [ 4, 5, 6]];
-
-//  for (let i = 1; 1 < array2D.length; i++) {
-
-//  console.log(array2D[i]);
-
-//}
-
-
-let finances = [
-  ['Jan-2010', 867884],
-  ['Feb-2010', 984655],
-  ['Mar-2010', 322013],
-  ['Apr-2010', -69417],
-  ['May-2010', 310503],
-  ['Jun-2010', 522857]
-];
-
-let financesUpdate = [
-  ['Jan-2010', 867884],
-  ['Feb-2010', 984655],
-  ['Mar-2010', 322013],
-  ['Apr-2010', -69417],
-  ['May-2010', 310503],
-  ['Jun-2010', 522857]
-];
-
-console.table(finances)
-//console.log('Array length is:',finances.length);
+// Determine greatest Increase / Decrease from the Updated P&L change array
 
 
 // Declare variables 
@@ -55,111 +200,113 @@ let monthTotal;
 let profitLoss = [];
 let month = [];
 let monthArray = [];
-let plArray = [];      // Yes
-// let plNum = 0;
-// let plTotal = 0;
-// let profitArray = [];
-// let lossArray = [];
-let netTotal =0;
+let plArray = [];
+let profitArray = [];
+let lossArray = [];
+let netTotal = 0;
 var differencesArray = [];
 
-/* Redundant code 
-// ** Function Declarations **
-//Function to add up array elements
-function addUp(monthlyPL) {
-  var xx, totalOfArray = 0;
-  for (xx = 0; xx < monthlyPL.length; xx++) {  
-    totalOfArray = totalOfArray + monthlyPL[xx];
-  }
-  return totalOfArray;
-}
-*/
-
+// --------------------------------------------------------------
+// ******* Calculate Total Profit and extract Months also P&L into arrays 
 for (let i = 0; i < finances.length; i++) {
 
   // Extarct the profit-loss only 
   let profitLoss = finances[i][1];
   //console.log('profit loss is: ', profitLoss);
   
+  // Calculate the Net Profit 
   netTotal += finances[i][1];
-   
+
   // Extract the finanical month only
   let month = finances[i][0];
   //console.log('finanial month is: ', month);
- 
-  // --------------------------------------------------------------
+
+  // -------------------------------------------------------------- 
   // ******* Months *******
 
   // 1. Split string into individual words 
   let substrings = month.split(' ');
   //console.table(substrings);
- 
+
   // 2. Create new array iterate thro list and push into array
   for (let j = 0; j < substrings.length; j++) {
-      monthArray.push(substrings[j]);        
+    monthArray.push(substrings[j]);
   }
   // --------------------------------------------------------------
   //  ****** Profit & Loss ******
 
   // 3. First convert number to a string   
-    let strNum = profitLoss.toString();
+  let strNum = profitLoss.toString();
 
-   // Split string into individual words 
-    //console.log(strNum.split(' '));
+  // Split string into individual words 
+  //console.log(strNum.split(' '));
 
-    let subnumbersPL = strNum.split(' ');
-    // console.table(subnumbersPL);
-  
+  let subnumbersPL = strNum.split(' ');
+  // console.table(subnumbersPL);
+
   // 4. Create new array iterate thro list and push into array
-    for (var k = 0; k < subnumbersPL.length; k++) {
-
-        plArray.push(subnumbersPL[k]);   
-    }
-        plArray.push(subnumbersPL[k]);   
-
-  } // *** End Main - For loop ***
-
-// Iterate through the finances array 
-  var totalChange = 0;
-  var change = 0;
-
-  // console.log(typeof());
-
-  for (var m = 0; m < finances.length-1; m++) {
-    
-    //if (m > 0 && m < finances.length-1) {
-     // console.log(typeof (m > 0 || m < finances.length-1));
-      change = finances[m][1] - finances[m+1][1];
-      // console.log(typeof (m > 0 || m < finances.length-1));
-
-      // Create 'changes differences)' Array
-      differencesArray.push(change);
-
-      console.log('count', m);
-      console.log('First item in array ', finances[m][1]);
-      console.log('Second item in array ', finances[m+1][1])
-      totalChange += change;
-    //} 
+  for (let k = 0; k < subnumbersPL.length; k++) {
+    plArray.push(subnumbersPL[k]);   
   }
+
+} // *** End Main - For loop ***
+
+// --------------------------------------------------------------
+// Iterate through the finances array to find Average change in Profit 
+var totalChange = 0;
+var change = 0;
+
+// Start with array [1], subatract from array [0]
+for (var m = 0; m < finances.length-1; m++) {
+  
+    change = finances[m][1] - finances[m+1][1];
+
+    // Create 'changes differences)' Array
+    differencesArray.push(change);
+
+    // Sum up the Total change over the period
+    totalChange += change;
+}
 
 // Calculate the avaerage change in Profit/Losses 
 var avgChange = totalChange / (finances.length - 1);
 
-//console.log(monthArray);  
-//console.log(differencesArray);
 
-// Updating 'finances array' to reflect differences
+// --------------------------------------------------------------
+ // ****** Profit & Loss Calculation ******
+
+  // 5. Covert String array into Integer, then Loop thro array to sum elements
+
+  for (let m = 0; m < plArray.length; m++) {
+
+    // Convert array to Integer
+    plNum = parseInt(plArray[m]);
+
+    // Check array element is a -ve number
+    if ((Math.sign(plNum)) == "-1") {
+      // Store -ve number in lossArray
+      lossArray.push(plNum);
+
+    } else { 
+      // Store +ve number in profitArray
+      profitArray.push(plNum);
+    } 
+
+  } // Profit & Loss - End 
+
+// --------------------------------------------------------------
+// ===== Updating 'finances array' to reflect differences =====
 // Initialise first element of Profit/Loss array zero
 financesUpdate[0][1] = '0';
 for (var n=0; n < financesUpdate.length; n++) {
   if (n > 0) {
     financesUpdate[n][1] = differencesArray[n-1];
   } 
-}
-console.table(financesUpdate);  
+} // Updating 'finances array' - End 
 
 
-// Searching "financesUpdate" for Greatest Increase 
+// --------------------------------------------------------------
+// ==== Searching "financesUpdate" for Greatest Increase ====
 
   // Set first value in Array to test as MAX
   let maxVal = financesUpdate[0][1];
@@ -167,19 +314,17 @@ console.table(financesUpdate);
 
 // console.log(financesUpdate[1][0]);
 
-  for (var index1 = 0; index1 < financesUpdate.length; index1++) {
+for (var index1 = 0; index1 < financesUpdate.length; index1++) {
 
-    // Loop to check all values in array against MAX found
-    if (financesUpdate[index1][1] > maxVal) {
-      maxVal = financesUpdate[index1][1];             // Test next value in array =
-      maxMonth = financesUpdate[index1][0];
-    }
+// Loop to check all values in array against MAX found
+  if (financesUpdate[index1][1] > maxVal) {
+    maxVal = financesUpdate[index1][1];             // Test next value in array =
+    maxMonth = financesUpdate[index1][0];
   }
-  // Output final result
-  console.log('Max value in Profit Array is ', maxVal);
-  console.log('Which occurs during Month ', maxMonth);
+} // Increase - End
 
-// Searching "financesUpdate" for Greatest Decrese
+// --------------------------------------------------------------
+// ===== Searching "financesUpdate" for Greatest Decrese =====
 
  // Set first value in Array to test as MIN
 let minVal = financesUpdate[0][1];
@@ -187,173 +332,20 @@ let minMonth = financesUpdate[0][0];
 
 for (var index2 = 0; index2 < financesUpdate.length; index2++) {
 
-   // Loop to check all values in array against MIN found
+  // Loop to check all values in array against MIN found
   if (financesUpdate[index2][1] < minVal) {
     minVal = financesUpdate[index2][1];             // Test next value in array =
     minMonth = financesUpdate[index2][0];
   }
-}
- // Output final result
-console.log('Min value in Profit Array is ', minVal);
-console.log('Which occurs during Month ', minMonth);
+} // Decrease - End
 
-
-
-/*
-let searchString = 'Feb-2010';
-let flag = false;
-let result = 0;
-
-for (var index = 0; index < financesUpdate.length; index++) {
- //console.log(financesUpdate[index][0]);
- // console.log(typeof(financesUpdate[index][0]));
- // console.log(typeof(searchString));
-
-  if (searchString == financesUpdate[index][0]) {
-    flag = true;
-    result = index;
-    break;
-  }
-}
- 
-  if (flag == true) {
-    console.log('Found on index: ', + index);
-    console.log('Profits ', financesUpdate[result][0]);
-  } else {
-    console.log('Index No not found');
-  }
-
-*/
-
-/*
-// Finding months - Increase / Decrease
-console.log(change);
-var greatestIncrease = 1926159;
-
-let count = 0;
-let monthIncrese = finances[change.indexOf(greatestIncrease) + 1][0];
-
-while(monthIncrese !== -1) {
-  count++;
-  monthIncrese = finances[change.indexOf(greatestIncrease) + 1][0];
-}
-
-console.log(monthIncrese);
-console.log(count);
-*/
-
-
-// console.log(totalChange);
-
-/* Redundant code 
-  // Profit & Loss Calculation 
-  // 5. Covert String array into Integer, then Loop thro array to sum elements
-
-      for (let m = 0; m < plArray.length; m++) {
-
-        // Convert array to Integer
-        plNum = parseInt(plArray[m]);
-
-        // Check array element is a -ve number
-        if ((Math.sign(plNum)) == "-1") {
-          // Store -ve number in lossArray
-          lossArray.push(plNum);
-        } else { 
-          // Store +ve number in profitArray
-          profitArray.push(plNum);
-        }
-        // Calculate Total of the porfit array
-        var totalIs = addUp(profitArray); 
-      }
-      console.log(profitArray);
-    // console.log('Sum Total is: ' + totalIs);
-*/
-  
-//console.log(monthArray);
-//console.log(monthArray.length);
-
+// --------------------------------------------------------------
 // ****** Console Output *********
 console.log('Financial Analysis');
 console.log('--------------------------');
 console.log('Total Months: ' + monthArray.length);
-
 console.log('Net Total: $'+ netTotal);
 console.log('Average Change: $' + avgChange.toFixed(2));
 
-
- //console.log(plTotal);
-//console.log(differencesArray);
-
-
-//console.log('total change over period: '+ totalChange); 
-
-
-
-
-
- // Using split command 
- // let substrings = month.split(' ');
- // console.log(substrings);
-
-
-
-  //console.table(substrings);
-
-//function arrayFromArgs() {
-//   var results = [];
-
-//   for (var i = 0; i < arguments.length; i++) {
-//      results.push(arguments[i]);
-//   }
-//   return results;
-//}
-
-// var monthArray = arrayFromArgs(month);
-// console.log(monthArray);
-//  console.table(monthArray);
-
-
-  //console.log('month array length is', month.length);
-
-  //monthTotal = (month.length);
-
-  //console.log(monthTotal);
-
-  //console.log(finances[i][1]);
-//}
-
-
-// let activities = [];
-//let activities = [  
-
-//  ['work', 9],
-//  ['Eat', 1],
-//  ['Commute', 2],
-//  ['Play Game', 1],
-//  ['Sleep', 7]
-//];
-
-// console.table(activities)
-
-// Print out the length of the array
-//console.log('Array length is:',activities.length);
-
-// loop the outer array 
-// for (let i = 0; i < activities.length; i++) {
-    
-    // Print out the inner array 
-      // console.log(activities[i]);
-
-    // get the size of the inner array 
-
-//    var innerArrayLength = activities[i].length;
-
-    // loop the inner array 
-
-//    for (let j = 0; j < innerArrayLength; j++) {
-
-//      console.log('[' + i + ',' + j + '] = ' + activities[i][j]);
-//    }
-//}
-
-
+console.log('Greatest Increase in Profits: ', maxMonth, '$'+ maxVal);
+console.log('Greatest Decrease in Profits: ', minMonth, '$'+ minVal);
